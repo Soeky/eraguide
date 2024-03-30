7fd974dd276d4ff226e191cf5b22d9502da4aa39
Soeky <seymeny527@gmail.com>
2024-03-31 00:08:44 +0100

    added input box

--- src/Components/Befehl/Befehl.jsx
+++ src/Components/Befehl/Befehl.jsx
@@ -1,11 +1,12 @@
 
+import Input from "antd/es/input/Input";
 import React from "react";
 import "../../App.css";
 
 function Befehl() {
 	return (
-		<div>
-			Befehl
+		<div className="InputTopLeft">
+			<Input placeholder="Input RISC-V Command" size="large" ></Input>
 		</div>
 	)
 }
