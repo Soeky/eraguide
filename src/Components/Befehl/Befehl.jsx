fd03e893580b043c3b25bf06240751ad9cac0d9c
Soeky <seymeny527@gmail.com>
2024-04-04 00:00:55 +0200

    buttons etc eingefuegt todos bei Befehl.jsx

--- src/Components/Befehl/Befehl.jsx
+++ src/Components/Befehl/Befehl.jsx
@@ -1,12 +1,35 @@
 
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Upload } from "antd";
 import Input from "antd/es/input/Input";
-import React from "react";
+import TextArea from "antd/es/input/TextArea";
+import React, { useState } from "react";
 import "../../App.css";
 
+
+
+
 function Befehl() {
+	const [outputCommands, setOutputCommands] = useState('');
+
+	//TODO verhalten bei einfuegen von text
+	//TODO file input handle
+
 	return (
-		<div className="InputTopLeft">
-			<Input placeholder="Input RISC-V Command" size="large" ></Input>
+		<div className="BefehlInputContainer">
+			<div className="BefehlInputSection">
+				<TextArea placeholder="Input RISC-V Commands"></TextArea>
+				<Button icon={<UploadOutlined />} type="primary" onClick={buttonCompileCommands}>Compile RISC-V Commands</Button>
+			</div>
+			<div className="BefehlFileSection">
+				<Upload multiple={false}>
+					<Button icon={<UploadOutlined />}>Upload Assembly file (.s)</Button>
+				</Upload>
+				<Button type="primary">Compile RISC-V file</Button>
+			</div>
+			<div className="BefehlOutputSection">
+				<p>{outputCommands}</p>
+			</div>
 		</div>
 	)
 }
